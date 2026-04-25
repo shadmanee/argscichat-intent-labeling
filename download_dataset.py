3346e80a760be232114037451964c56271775e62
Shadmanee <tshadmanee@gmail.com>
2026-04-25 18:02:39 +0600

    fetching dataset directly

---
+++ download_dataset.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/federicoruggeri/argscichat_project/refs/heads/main/argscichat_allennlp/argscichat_intents/dataset.csv"
+output_dir = Path("./data/")
+
+output_dir.mkdir(exist_ok=True, parents=True)
+
+output_path = output_dir / "dataset.csv"
+
+df = pd.read_csv(url)
+df.to_csv(output_path, index=False)
